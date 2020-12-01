77441fb9f7b30c5eda131a188aa48c8c8f32573a
Chris Gresty <chris.gresty@gmail.com>
2020-12-01 09:02:41 +0000

    Day 1

---
+++ src/main/java/dev/gresty/aoc2020/Day01.java
@@ -0,0 +1,39 @@
+package dev.gresty.aoc2020;
+
+import static dev.gresty.aoc2020.Utils.msg;
+import static dev.gresty.aoc2020.Utils.withLines;
+
+public class Day01 {
+
+    public static void main(String[] args) {
+        int[] expenses = withLines("day01.txt",
+                s -> s.mapToInt(Integer::parseInt).toArray());
+
+        msg("Puzzle 1: " + puzzle1(expenses));
+        msg("Puzzle 2: " + puzzle2(expenses));
+    }
+
+    static int puzzle1(int[] expenses) {
+        for (int i = 0; i < expenses.length - 1; i++) {
+            for (int j = i + 1; j < expenses.length; j++) {
+                if (expenses[i] + expenses[j] == 2020) {
+                    return expenses[i] * expenses[j];
+                }
+            }
+        }
+        return -1;
+    }
+
+    static int puzzle2(int[] expenses) {
+        for (int i = 0; i < expenses.length - 2; i++) {
+            for (int j = i + 1; j < expenses.length - 1; j++) {
+                for (int k = j + 1; k < expenses.length; k++) {
+                    if (expenses[i] + expenses[j] + expenses[k] == 2020) {
+                        return expenses[i] * expenses[j] * expenses[k];
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/main/java/dev/gresty/aoc2020/Utils.java
@@ -0,0 +1,35 @@
+package dev.gresty.aoc2020;
+
+import java.io.*;
+import java.net.URL;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class Utils {
+
+    public static void msg(String msg) {
+        System.out.println(msg);
+    }
+
+    public static <T> T withLines(String filename, Function<Stream<String>, T> function) {
+        try (var in = readFile(filename)) {
+            return function.apply(in.lines());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+//    public static InputStream streamFile(String filename) throws IOException {
+//        return resource(filename).openStream();
+//    }
+
+    public static BufferedReader readFile(String filename) throws IOException {
+        return new BufferedReader(new InputStreamReader(resource(filename).openStream()));
+    }
+
+    public static URL resource(String filename) throws FileNotFoundException {
+        URL url = Utils.class.getClassLoader().getResource("dev/gresty/aoc2020/" + filename);
+        if (url == null) throw new FileNotFoundException("dev/gresty/aoc2020/" + filename);
+        return url;
+    }
+}
